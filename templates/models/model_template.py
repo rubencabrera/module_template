e79ab17091148351b0335dd05efeb55c78fa738b
Rubén Cabrera <dev@rubencabrera.es>
2016-10-04 14:06:48 +0200

    Model template

---
+++ templates/models/model_template.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright $year $autor <$email_autor>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+# Imports habituales
+from openerp import import api, fields, models
+
